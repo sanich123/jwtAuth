f7b9636a397100d7801b836115a8b910d922a14e
sanich123 <aovoronin.piano@gmail.com>
2023-03-13 18:08:45 +0300

    feat: another try

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-const { start } = require("./init.js");
-// const { router } = require("./auth-router.js");
-// const { Routes } = require("./const.js");
-// const PORT = process.env.PORT || 3000;
-// const app = express();
+const express = require("express");
+const mongoose = require("mongoose");
+const {MONGO_DB_CONNECTION} = require('./const.js');
+const { router } = require("./auth-router.js");
+const { Routes } = require("./const.js");
+const PORT = process.env.PORT || 3000;
+const app = express();
 
-// app.use(express.json());
+app.use(express.json());
 
-// app.get("/", (req, res) => {
-//   res.send("Hello world");
-// });
-// app.use(Routes.auth, router);
+app.use(Routes.auth, router);
 
+async function start() {
+  try {
+    await mongoose.connect(MONGO_DB_CONNECTION);
+    app.listen(PORT, () =>
+      console.log(`server was started at the port ${PORT}`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
 start();
-
-// module.exports = app;
+module.exports = app;
